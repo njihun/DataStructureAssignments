93e1435cb918a9fdea354c6a152e3459bb4e2f10
Lee Juyoung <adelinewtro@gmail.com>
2026-05-26 17:05:54 +0900

    .

--- Task/src/SortCase/ArraysSort.java
+++ Task/src/SortCase/ArraysSort.java
@@ -1,8 +1,10 @@
 package SortCase;
 
+import java.util.Arrays;
+
 public class ArraysSort implements SortCaseInterface {
     @Override
     public void sort(Comparable[] array) {
-
+        Arrays.sort(array);
     }
 }
--- Task/src/SortCase/CollectionSort.java
+++ Task/src/SortCase/CollectionSort.java
@@ -1,8 +1,13 @@
 package SortCase;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class CollectionSort implements SortCaseInterface {
     @Override
     public void sort(Comparable[] array) {
-
+        List<Comparable> values = Arrays.asList(array);
+        Collections.sort(values);
     }
 }
